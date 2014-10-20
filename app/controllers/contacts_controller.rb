13f23f5c916a109b0c3b10a59eed18dc19d095c9
Charles Bilyue <ckbilyue@gmail.com>
2014-10-20 14:19:26 -0500

    save data to a spreadsheet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,5 @@
 class ContactsController < ApplicationController
 
-  def process_form
-    Rails.logger.debug "DEBUG: params are #{params}"
-    flash[:notice] = "Received request from #{params[:contact][:name]}"
-    redirect_to root_path
-  end
-
-end
-
-class ContactsController < ApplicationController
-
   def new
     @contact = Contact.new
   end
@@ -17,7 +7,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-      # TODO save data
+      @contact.update_spreadsheet
       # TODO send message
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
@@ -31,4 +21,5 @@ class ContactsController < ApplicationController
   def secure_params
     params.require(:contact).permit(:name, :email, :content)
   end
+
 end
